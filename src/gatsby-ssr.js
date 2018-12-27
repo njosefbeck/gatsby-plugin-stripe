65de2ea7a0dc6011a194dd7ce7175108b01af5c6
Nathan Beck <nathan.beck@fleishman.com>
2018-12-27 13:56:48 -0600

    Add id to script tag

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -3,6 +3,7 @@ import React from 'react';
 exports.onRenderBody = ({ setPostBodyComponents }, options) => {
   return setPostBodyComponents([
     <script
+      id="stripe-js"
       key="gatsby-plugin-stripe"
       src="https://js.stripe.com/v3/"
       async={options.async}
